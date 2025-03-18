f4d541f9bd514b8ae8bf98eeccee79140598b7d5
Łukasz Czajka <lukasz.czajka48@gmail.com>
2025-03-18 21:30:42 +0100

    initial commit

---
+++ src/main/java/com/demo/reportconv/ReportconvApplication.java
@@ -0,0 +1,13 @@
+package com.demo.reportconv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportconvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportconvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/reportconv/ReportconvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.reportconv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportconvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
